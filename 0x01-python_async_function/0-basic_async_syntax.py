3d3aed8da028669612e28a725ef6048ae051908a
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-15 00:22:18 +0200

    pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Basic async syntax """
+""" Basic async syntax with await """
 import asyncio
 import random
 
@@ -8,15 +8,13 @@ async def wait_random(max_delay):
     """
     Asynchronous coroutine taht waits
     for a random amount of time between 0 and max_delay
-    
     Args:
         max_delay  (int): Maximum delay in seconds
 
     Return:
         Float: delay in seconds
-
     """
 
-    delay =  random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
