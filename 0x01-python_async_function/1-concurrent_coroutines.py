e60f3d8f6cd6a8ab4c28b3eed866c92f95437fa7
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-15 01:19:55 +0200

    new

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 """ Function that imports code from prev file """
 import asyncio
+import random
 from typing import List
 
-wait_random = __import__("0-basic_async_syntax").wait_random
 
+async def wait_random(max_delay: int) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
-async def wait_n(n: int, max_delay: int) -> List:
-    """ Function that waits n times and returns a list of delays """
-
+async def wait_n(n: int, max_delay: int) -> list:
+    """ Create a list of tasks using wait_random """
     tasks = [wait_random(max_delay) for _ in range(n)]
-
+    
+    # Use asyncio.as_completed to get the results in the order they finish
     delays = []
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-
+    
     return delays
