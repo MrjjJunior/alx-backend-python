2ae19e644c22183a75568ec29c0068e464389b69
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-15 01:12:02 +0200

    Lists

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ Function that imports code from prev file """
 import asyncio
-import importlib
 from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     """ Function that waits n times and returns a list of delays """
 
     tasks = [wait_random(max_delay) for _ in range(n)]
