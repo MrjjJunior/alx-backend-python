ec2c0cf24513f59fb07a281168489d61fded23cc
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-04 13:14:38 +0200

    Test of Raise error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,5 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Any
                                ) -> None:
         """ Test access nested map """
-
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Dict[str, Any],
+                                         path: Tuple[str]
+                                         ) -> None:
+        ''' Tests that a KeyError is raised when the path is invalid '''
+        with self.assertRaises(KeyError) as error_handler:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(error_handler.exception), f"'{path[-1]}'")
